13825ebd92fbfaf003535aab231cd12f3c0425ee
KayanAPro <142297819+KayanAPro@users.noreply.github.com>
2023-12-12 16:39:27 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d")
+
+ctx.beginPath();
+ctx.strokestyle = "red";
+ctx.linewidth = 1;
+ctx.arc(150, 143, 430, 200);
+ctx.stroke;
